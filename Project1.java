cb471adee134ed2325541a55fd62fd830d298141
SuprajaKolasani <suprajakolasani@gmail.com>
2026-08-10 11:26:24 +0530

    second commit

---
+++ Project1.java
@@ -0,0 +1,11 @@
+package myfirstcorejavaproject;
+
+public class Project1 {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		System.out.println("Heloo");
+		System.out.println(Class.forName("com.mysql.cj.jdbc.Driver"));
+	}
+
+}
